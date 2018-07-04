e9a1323f38ec2db8f1d2b1922ae93aa28f26f152
warlock921 <caoyu921@163.com>
2018-07-04 19:33:47 +0800

    数据库模型建立

--- ems_mainsite/admin.py
+++ ems_mainsite/admin.py
@@ -1,18 +1,14 @@
 from django.contrib import admin
-from .models import CompanyInfo, CompanyInfoOverHead, CompanyProduct, CompanyTag, CompanyType, MyCompanyProductList, InternalCircular
+from ems_mainsite.models import CompanyInfo, CompanyInfoOverHead, CompanyTag, CompanyType, InternalCircular
 
 # Register your models here.
 @admin.register(CompanyInfo)
 class CompanyInfoAdmin(admin.ModelAdmin):
-    list_display = ['company_name', 'company_type', 'responsible_person', 'contact_name', 'contact_phone', 'contact_email', 'contact_QQ', 'contact_Ding', 'contact_address', 'create_time']
+    list_display = []
 
 @admin.register(CompanyInfoOverHead)
 class CompanyInfoOverHeadAdmin(admin.ModelAdmin):
     list_display = ['company_info_id', 'business_contact_flag']
-
-@admin.register(CompanyProduct)
-class CompanyProductAdmin(admin.ModelAdmin):
-    list_display = ['company_info_id', 'cproduct_name', 'cproducts_type', 'cproduct_market_price', 'cproduct_internal_price', 'cproduct_in_storage_date', 'cproduct_on_sale']
 
 @admin.register(CompanyTag)
 class CompanyTagAdmin(admin.ModelAdmin):
@@ -21,10 +17,6 @@ class CompanyTagAdmin(admin.ModelAdmin):
 @admin.register(CompanyType)
 class CompanyTypeAdmin(admin.ModelAdmin):
     list_display = ['company_type_name', ]
-
-@admin.register(MyCompanyProductList)
-class MyCompanyProductListAdmin(admin.ModelAdmin):
-    list_display = ['mproduct_name', 'mproducts_makers', 'mproduct_market_price', 'mproduct_internal_price', 'mproduct_in_storage_date', 'mproduct_update_time', 'mproduct_on_sale']
 
 @admin.register(InternalCircular)
 class InternalCircularAdmin(admin.ModelAdmin):
--- ems_mainsite/models.py
+++ ems_mainsite/models.py
@@ -1,54 +1,100 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-#我司售卖产品列表
-class MyCompanyProductList(models.Model):
-    mproduct_name = models.CharField(max_length=200, verbose_name="产品名称")
-    mproducts_makers = models.CharField(max_length=200, verbose_name="产商")
-    mproduct_market_price = models.IntegerField(verbose_name="市场价")
-    mproduct_internal_price = models.IntegerField(verbose_name="内部价")
-    mproduct_in_storage_date = models.DateTimeField(auto_now_add=True, verbose_name="入库时间")
-    mproduct_update_time = models.DateTimeField(auto_now=True, verbose_name="产品更新日期")
-    mproduct_on_sale = models.BooleanField(verbose_name="是否在售", default=True)
-
-    def __str__(self):
-        return self.mproduct_name
-    
-    class Meta:
-        ordering = ['-mproduct_in_storage_date', ]
-
 #企业类型表
 class CompanyType(models.Model):
-    company_type_name = models.CharField(max_length=50, verbose_name="公司类型")
+    company_type_name = models.CharField(max_length=50, verbose_name="企业产业分类")
 
     def __str__(self):
         return self.company_type_name
+
+    class Meta:
+        verbose_name_plural = "企业产业分类"
 
 
 
 #标签表
 class CompanyTag(models.Model):
+    
+    LEVEL_CHOICES = (
+        (1,"特别重要"),
+        (2,"非常重要"),
+        (3,"一般重要"),
+    )
+
     tag_name = models.CharField(max_length=50, verbose_name="标签名称")
-    tag_important_level = models.IntegerField(verbose_name="重要级别", default=3)
+    tag_important_level = models.IntegerField(choices=LEVEL_CHOICES, verbose_name="重要级别", default=3)
 
     def __str__(self):
         return self.tag_name
     
     class Meta:
         ordering = ['-tag_important_level', ]
+        verbose_name_plural = "企业标签"
 
 #企业信息表
 class CompanyInfo(models.Model):
-    company_name = models.CharField(max_length=200, verbose_name="公司名称")
-    company_type = models.ForeignKey(CompanyType, on_delete=models.CASCADE, related_name="company_type_id", verbose_name="公司类型")
-    responsible_person = models.CharField(max_length=50, verbose_name="企业法人/负责人")
-    contact_name = models.CharField(max_length=50, verbose_name="企业联系人姓名")
-    contact_phone = models.CharField(max_length=11, verbose_name="企业联系人电话")
-    contact_email = models.EmailField(verbose_name="企业联系人email")
-    contact_QQ = models.CharField(max_length=20, verbose_name="联系人QQ号")
-    contact_Ding = models.CharField(max_length=50, verbose_name="企业联系人钉钉号")
-    contact_address = models.CharField(max_length=200, verbose_name="企业通讯地址")
+    
+     COUNTY_CHOICES = (
+        (1,"个旧市"),
+        (2,"开远市"),
+        (3,"蒙自市"),
+        (4,"建水县"),
+        (5,"石屏县"),
+        (6,"弥勒市"),
+        (7,"泸西县"),
+        (8,"红河县"),
+        (9,"元阳县"),
+        (10,"绿春县"),
+        (11,"屏边县"),
+        (12,"金平县"),
+        (13,"河口县"),
+    )
+
+    SEX_CHOICES = (
+        (1,"男"),
+        (2,"女"),
+    )
+
+    POLITICS_CHOICES = (
+        (1,"党员"),
+        (2,"群众"),
+        (3,"其他"),
+    )
+
+    EDUCATION_CHOICES = (
+        (1,"初中"),
+        (2,"高中(中专)"),
+        (3,"大专"),
+        (4,"本科"),
+        (5,"本科以上"),
+    )
+
+    BOOL_CHOICES =(
+        (1,"是"),
+        (2,"否"),
+    )
+
+    company_name = models.CharField(max_length=200, verbose_name="企业名称")
+    company_type = models.ForeignKey(CompanyType, on_delete=models.CASCADE, related_name="company_type_id", verbose_name="企业产业分类")
+    company_area = models.IntegerField(choices=COUNTY_CHOICES, verbose_name="企业归属地", default=1)
+    company_IDcard = models.CharField(max_length=18, verbose_name="企业统一信用代码", blank=True)
+    company_business_scope = models.TextField(max_length=500, verbose_name="企业经营范围", blank=True)
+    company_registered_capital = models.CharField(max_length=50, verbose_name="企业注册资金", blank=True)
+    responsible_person = models.CharField(max_length=50, verbose_name="企业法人/负责人姓名")
+    responsible_person_sex = models.IntegerField(choices=SEX_CHOICES, verbose_name="企业法人性别", default=1)
+    responsible_person_age = models.CharField(max_length=2, verbose_name="企业法人年龄", blank=True)
+    responsible_person_politics_status = models.IntegerField(choices=POLITICS_CHOICES, verbose_name="企业法人政治面貌", default=2)
+    responsible_person_education = models.IntegerField(choices=EDUCATION_CHOICES, verbose_name="企业法人文化程度", default=3)
+    contact_name = models.CharField(max_length=50, verbose_name="企业联系人姓名", blank=True)
+    contact_phone = models.CharField(max_length=11, verbose_name="企业联系人电话", blank=True)
+    contact_email = models.EmailField(verbose_name="企业联系人email", blank=True)
+    company_web = models.URLField(verbose_name="企业网站地址", blank=True)
+    contact_address = models.CharField(max_length=200, verbose_name="企业通讯地址", blank=True)
+    company_cancel = models.IntegerField(choices=BOOL_CHOICES, verbose_name="公司是否注销", default=2)
+    #company_tag = models.ManyToManyField(CompanyTag ,related_name="company_info_to_tag")
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="录入系统时间")
 
     def __str__(self):
@@ -56,39 +102,40 @@ class CompanyInfo(models.Model):
 
     class Meta:
         ordering = ['-create_time', ]
+        verbose_name_plural = "企业基础信息"
 
-#企业产品列表
-class CompanyProduct(models.Model):
-    company_info_id = models.ForeignKey(CompanyInfo, on_delete=models.CASCADE, verbose_name="所属企业")
-    cproduct_name = models.CharField(max_length=200, verbose_name="产品名称")
-    cproducts_type = models.CharField(max_length=200, verbose_name="产品类别")
-    cproduct_market_price = models.IntegerField(verbose_name="市场价")
-    cproduct_internal_price = models.IntegerField(verbose_name="内部价")
-    cproduct_in_storage_date = models.DateTimeField(auto_now_add=True, verbose_name="入库时间")
-    cproduct_on_sale = models.BooleanField(verbose_name="是否在售", default=True)
-
-    def __str__(self):
-        return self.cproduct_name
-    
-    class Meta:
-        ordering = ['-cproduct_in_storage_date', ]
 
 #企业附加信息表
 class CompanyInfoOverHead(models.Model):
     company_info_id = models.OneToOneField(CompanyInfo, on_delete=models.CASCADE, verbose_name="所属企业")
     company_tag = models.ManyToManyField(CompanyTag, related_name="company_tag_for_company", verbose_name="企业标签")
-    business_contact_flag = models.BooleanField(verbose_name="是否与我司有业务往来", default=False)
-    from_mycompany_product = models.ManyToManyField(MyCompanyProductList, related_name="mycompany_from_product", verbose_name="正在使用的我司产品")
-    company_product = models.ManyToManyField(CompanyProduct, related_name="company_for_product", verbose_name="企业产品清单")
+    company_employee = models.IntegerField(max_length=5, verbose_name="企业从业人员规模", blank=True)
+    company_senior_staff = models.IntegerField(max_length=5, verbose_name="企业大专及以上学历人数", blank=True)
+    company_job_title = models.IntegerField(max_length=5, verbose_name="企业中级及以上职称人数", blank=True)
+    company_registered_capital = models.IntegerField(verbose_name="企业注册资金", blank=True)
+    company_patents_number = models.IntegerField(max_length=5, verbose_name="企业拥有专利个数", blank=True)
+    company_product = models.TextField(verbose_name="企业主要产品/服务", blank=True)
+    company_annual_income = models.IntegerField(verbose_name="企业年收入", blank=True)
+    company_remark = models.TextField(verbose_name="企业备注", blank=True)
 
     def __str__(self):
         return self.company_info_id
 
+    class Meta:
+        verbose_name_plural = "企业附加信息"
+
 
 #内置通知表
 class InternalCircular(models.Model):
+
+    LEVEL_CHOICES = (
+        (1,"特别重要"),
+        (2,"非常重要"),
+        (3,"一般重要"),
+    )
+
     notification_title = models.CharField(max_length=200, verbose_name="通知标题")
-    important_level = models.IntegerField(verbose_name="通知重要级别", default=3)
+    important_level = models.IntegerField(choices=LEVEL_CHOICES, verbose_name="通知重要级别", default=3)
     notification_content = models.TextField(verbose_name="通知内容")
     notification_author = models.CharField(max_length=50, verbose_name="通知作者")
     notification_date = models.DateField(auto_now_add=True, verbose_name="通知生成日期")
@@ -99,3 +146,4 @@ class InternalCircular(models.Model):
 
     class Meta:
         ordering = ['-notification_date', ]
+        verbose_name_plural = "内置通知表"
