e3780aeea9d2fd39e593e9e0a01dadcdd127b32e
warlock921 <caoyu921@163.com>
2018-07-20 01:24:32 +0800

    移除小BUG

--- ems_account/adminx.py
+++ ems_account/adminx.py
@@ -2,12 +2,11 @@ import xadmin
 from xadmin import views
 from .models import UserLevel, UserPermissionProfile
 
+@xadmin.sites.register(UserLevel)
 class UserLevelAdmin(object):
     list_display = ['level_name', 'level_describe']
 
-xadmin.site.register(UserLevel, UserLevelAdmin)
-
+@xadmin.sites.register(UserPermissionProfile)
 class UserPermissionProfileAdmin(object):
      list_display = ['user', 'user_level', 'user_phone', 'user_agent', 'user_ip']
 
-xadmin.site.register(UserPermissionProfile, UserPermissionProfileAdmin)
